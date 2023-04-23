ee2a8e8a42c954ce539b66f6a245c4fb3ed161f7
Gabriel Duarte <gabrielduartecarneiro@gmail.com>
2023-04-23 18:23:44 -0300

    Estilização da página e funcionalidade com Flask

--- app.py
+++ app.py
@@ -2,11 +2,25 @@ from flask import Flask, render_template, request
 import pokeapi as pk
 app = Flask(__name__)
 
+dados_pokemon = {
+        'nome' : 'charizard',
+        'id' : 6,
+        'tipo' : ['fire', 'flight'],
+        'sprite' : 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/6.png',
+        'spriteanimated' : 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/6.gif'
+    }
+
 @app.route('/')
-@app.route('/<name>')
-def pokedex(name = None):
+def index():
+    return render_template('home.html', dados_pokemon = dados_pokemon)
+
+@app.route('/process_form', methods = ['POST'])
+def process_form():
+    name = request.form['pokemon']
     dados_pokemon = pk.pega_pokemon(name)
-    return render_template('index.html', dados_pokemon = dados_pokemon)
-    
+    imagem1 = dados_pokemon['sprite']
+    imagem2 = dados_pokemon['spriteanimated']
+    return render_template('home.html', dados_pokemon = dados_pokemon, imagem1 = imagem1, imagem2 = imagem2)
+
 if __name__ == '__main__':
     app.run(debug=True)
--- pokeapi.py
+++ pokeapi.py
@@ -1,6 +1,8 @@
 import requests
 
 def pega_pokemon(name):
+    if name == None:
+        return ' '
     url = f'https://pokeapi.co/api/v2/pokemon/{name}'
     response = requests.get(url)
     if response.status_code == 200:
@@ -8,6 +10,10 @@ def pega_pokemon(name):
         dados_pokemon = {
             'nome' : data['name'],
             'id' : data['id'],
-            'sprite' : data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+            'tipo' : [],
+            'sprite' : data['sprites']['front_default'],
+            'spriteanimated' : data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
         }
+        for i in data['types']:
+            dados_pokemon['tipo'].append(i['type']['name'])
     return dados_pokemon
